fc37fd27a97334c6ed9066682fcb601e848297ec
Pere Villega <pere.villega@gmail.com>
2015-05-12 19:43:30 +0100

    Refactor Scala.Js code a bit to test things

--- frontend/src/main/scala/example/akkawschat/Frontend.scala
+++ frontend/src/main/scala/example/akkawschat/Frontend.scala
@@ -8,13 +8,16 @@ import org.scalajs.dom
 object Frontend extends js.JSApp {
   val joinButton = dom.document.getElementById("join").asInstanceOf[HTMLButtonElement]
   val sendButton = dom.document.getElementById("send").asInstanceOf[HTMLButtonElement]
+  val nameField = dom.document.getElementById("name").asInstanceOf[HTMLInputElement]
+  val messageField = dom.document.getElementById("message").asInstanceOf[HTMLInputElement]
+  val playground = dom.document.getElementById("playground")
 
   def main(): Unit = {
-    val nameField = dom.document.getElementById("name").asInstanceOf[HTMLInputElement]
     joinButton.onclick = { (event: MouseEvent) ⇒
       joinChat(nameField.value)
       event.preventDefault()
     }
+
     nameField.focus()
     nameField.onkeypress = { (event: KeyboardEvent) ⇒
       if (event.keyCode == 13) {
@@ -22,25 +25,26 @@ object Frontend extends js.JSApp {
         event.preventDefault()
       }
     }
+
+    messageField.onkeypress = { (event: KeyboardEvent) ⇒
+      if (event.keyCode == 13) {
+        sendButton.click()
+        event.preventDefault()
+      }
+    }
   }
 
   def joinChat(name: String): Unit = {
     joinButton.disabled = true
-    val playground = dom.document.getElementById("playground")
     playground.innerHTML = s"Trying to join chat as '$name'..."
+
+    // configure websocket
     val chat = new WebSocket(getWebsocketUri(dom.document, name))
     chat.onopen = { (event: Event) ⇒
       playground.insertBefore(p("Chat connection was successful!"), playground.firstChild)
-      sendButton.disabled = false
-
-      val messageField = dom.document.getElementById("message").asInstanceOf[HTMLInputElement]
       messageField.focus()
-      messageField.onkeypress = { (event: KeyboardEvent) ⇒
-        if (event.keyCode == 13) {
-          sendButton.click()
-          event.preventDefault()
-        }
-      }
+
+      sendButton.disabled = false
       sendButton.onclick = { (event: Event) ⇒
         chat.send(messageField.value)
         messageField.value = ""
@@ -50,28 +54,33 @@ object Frontend extends js.JSApp {
 
       event
     }
+
     chat.onerror = { (event: ErrorEvent) ⇒
-      playground.insertBefore(p(s"Failed: code: ${event.colno}"), playground.firstChild)
-      joinButton.disabled = false
-      sendButton.disabled = true
+      closeChatMessage(s"Failed: code: ${event.colno}")
     }
+
     chat.onmessage = { (event: MessageEvent) ⇒
       playground.insertBefore(p(event.data.toString), playground.firstChild)
     }
+
     chat.onclose = { (event: Event) ⇒
-      playground.insertBefore(p("Connection to chat lost. You can try to rejoin manually."), playground.firstChild)
-      joinButton.disabled = false
-      sendButton.disabled = true
+      closeChatMessage("Connection to chat lost. You can try to rejoin manually.")
     }
   }
 
-  def getWebsocketUri(document: Document, nameOfChatParticipant: String): String = {
+  private def closeChatMessage(msg: String) = {
+    playground.insertBefore(p(msg), playground.firstChild)
+    joinButton.disabled = false
+    sendButton.disabled = true
+  }
+
+  private def getWebsocketUri(document: Document, nameOfChatParticipant: String): String = {
     val wsProtocol = if (dom.document.location.protocol == "https:") "wss" else "ws"
 
     s"$wsProtocol://${dom.document.location.host}/chat?name=$nameOfChatParticipant"
   }
 
-  def p(msg: String) = {
+  private def p(msg: String): Element = {
     val paragraph = dom.document.createElement("p")
     paragraph.innerHTML = msg
     paragraph
